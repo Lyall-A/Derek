6374afe44c42df584da73f6ca7f25867b1b3416d
Lyall <me@lyall.lol>
2024-10-07 19:05:35 +0100

    a

--- Derekputer/deploy.sh
+++ Derekputer/deploy.sh
@@ -10,36 +10,68 @@ if [ -z "$(command -v swig)" ]; then echo "swig is not installed!"; exit 1; fi
 # jobs=$(nproc)
 jobs=4
 
-echo "Downloading Debian stable..."
-sudo debootstrap --foreign --arch=arm64 stable Derek-OS http://deb.debian.org/debian/
+download() {
+    name=$1
+    dir=$2
+    command=$3
+    if [ ! -d "$dir" ]; then
+        echo "Building $name..."
+        $command
+    else
+        echo "Already downloaded $name"
+    fi
+}
 
-echo "Downloading Linux source..."
-git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git Linux
+config() {
+    name=$1
+    config_name=$2
+    echo "Applying $name config..."
+    sudo make -s ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- $config_name
+}
 
-echo "Downloading ARM Trusted Firmware source..."
-git clone https://github.com/ARM-software/arm-trusted-firmware.git ARM-Trusted-Firmware
+build() {
+    name=$1
+    file=$2
+    make_args=$3
+    if [ ! -f "$file" ]; then
+        echo "Building $name..."
+        sudo make CROSS_COMPILE=aarch64-linux-gnu- $make_args -j$(jobs)
+    else
+        echo "$name Already built"
+    fi
+}
 
-echo "Downloading U-Boot source..."
-git clone https://github.com/u-boot/u-boot.git U-Boot
+# Download Debian stable
+download "Debian stable" "Derek-OS" "sudo debootstrap --foreign --arch=arm64 stable Derek-OS http://deb.debian.org/debian/"
 
-echo "Building Linux uncompressed kernel image and device tree blobs..."
+# Download Linux source
+download "Linux" "Linux" "git clone https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git Linux"
+
+# Download ARM Trusted Firmware source
+download "ARM Trusted Firmware" "ARM-Trusted-Firmware" "git clone https://github.com/ARM-software/arm-trusted-firmware.git ARM-Trusted-Firmware"
+
+# Download U-Boot source
+download "U-Boot" "U-Boot" "git clone https://github.com/u-boot/u-boot.git U-Boot"
+
+# Build required Linux files
 cd Linux
-sudo make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- defconfig # NOTE: may need adjusted slightly, or just copied from armbian
-sudo make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image dtbs -j$(jobs)
+config "Linux" "defconfig" # NOTE: may need adjusted slightly, or just copied from armbian
+build "Linux DTB's" "arch/arm64/boot/Image" "Image"
+build "Linux image" "arch/arm64/boot/dts/allwinner/sun50i-h618-orangepi-zero3.dtb" "dtbs"
 cp arch/arm64/boot/Image ..
 cp arch/arm64/boot/dts/allwinner/sun50i-h618-orangepi-zero3.dtb ..
 cd ..
 
-echo "Building ARM Trusted Firmware..."
+# Build required ARM Trusted Firmware files
 cd ARM-Trusted-Firmware
-sudo make CROSS_COMPILE=aarch64-linux-gnu- PLAT=sun50i_a64 DEBUG=0 bl31 -j$(jobs)
+build "ARM Trusted Firmware" "build/sun50i_a64/release/bl31.bin" "PLAT=sun50i_a64 DEBUG=0 bl31"
 cp build/sun50i_a64/release/bl31.bin ../BL31.bin
 cd ..
 
-echo "Building U-Boot..."
+# Build required U-Boot files
 cd U-Boot
-sudo make CROSS_COMPILE=aarch64-linux-gnu- orangepi_zero3_defconfig
-sudo make CROSS_COMPILE=aarch64-linux-gnu- BL31=../BL31.bin -j$(jobs)
+config "U-Boot" "orangepi_zero3_defconfig"
+build "U-Boot" "u-boot-sunxi-with-spl.bin" "BL31=../BL31.bin"
 cp u-boot-sunxi-with-spl.bin ..
 cd ..
 
